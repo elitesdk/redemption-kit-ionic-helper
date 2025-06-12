dcf54130da0ad9bbebca1d5c16626f04e4d8b78b
Prasad Parab <prasad.parab@loylty.com>
2025-06-12 12:45:21 +0530

    Added functions for Offers, EGV, BBPS

--- ios/Sources/RedemptionKitIonicHelperPlugin/RedemptionKitIonicHelper.swift
+++ ios/Sources/RedemptionKitIonicHelperPlugin/RedemptionKitIonicHelper.swift
@@ -1,7 +1,67 @@
 import Foundation
 
 @objc public class RedemptionKitIonicHelper: NSObject {
+    @objc public func initialize(_ value: String) -> String {
+        print(value)
+        return value
+    }
+
+    @objc public func launch(_ value: String) -> String {
+        print(value)
+        return value
+    }
+
     @objc public func initializeAndLaunch(_ value: String) -> String {
+        print(value)
+        return value
+    }
+
+    @objc public func getPointBalance(_ value: String) -> String {
+        print(value)
+        return value
+    }
+
+    @objc public func getTransactionHistory(_ value: String) -> String {
+        print(value)
+        return value
+    }
+
+    @objc public func getRedemptionOptions(_ value: String) -> String {
+        print(value)
+        return value
+    }
+
+    @objc public func getBanners(_ value: String) -> String {
+        print(value)
+        return value
+    }
+
+    @objc public func getGiftVouchers(_ value: String) -> String {
+        print(value)
+        return value
+    }
+
+    @objc public func getOfferCategories(_ value: String) -> String {
+        print(value)
+        return value
+    }
+
+    @objc public func getOffers(_ value: String) -> String {
+        print(value)
+        return value
+    }
+
+    @objc public func getBillPayCategories(_ value: String) -> String {
+        print(value)
+        return value
+    }
+
+    @objc public func searchProducts(_ value: String) -> String {
+        print(value)
+        return value
+    }
+
+    @objc public func clearSession(_ value: String) -> String {
         print(value)
         return value
     }
--- ios/Sources/RedemptionKitIonicHelperPlugin/RedemptionKitIonicHelperPlugin.swift
+++ ios/Sources/RedemptionKitIonicHelperPlugin/RedemptionKitIonicHelperPlugin.swift
@@ -11,31 +11,27 @@ public class RedemptionKitIonicHelperPlugin: CAPPlugin, CAPBridgedPlugin {
     public let identifier = "RedemptionKitIonicHelperPlugin"
     public let jsName = "RedemptionKitIonicHelper"
     public let pluginMethods: [CAPPluginMethod] = [
-        CAPPluginMethod(name: "initializeAndLaunch", returnType: CAPPluginReturnPromise)
+        CAPPluginMethod(name: "initialize", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "launch", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "initializeAndLaunch", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "getPointBalance", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "getTransactionHistory", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "getRedemptionOptions", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "getBanners", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "getGiftVouchers", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "getOfferCategories", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "getOffers", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "getBillPayCategories", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "searchProducts", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "clearSession", returnType: CAPPluginReturnPromise)
     ]
     private let implementation = RedemptionKitIonicHelper()
 
-    @objc func initializeAndLaunch(_ call: CAPPluginCall) {
-//         let value = call.getString("value") ?? ""
-//        
-//        DispatchQueue.main.async {
-//            let vc = CustomViewController(sso: value)
-//            self.bridge?.viewController?.present(vc, animated: true)
-//        }
-//        
-//        call.resolve([
-//            "value": value
-//        ])
-        
+    @objc
+    func initialize(_ call: CAPPluginCall) {
         Task {
             guard let apiKey = call.getString("apiKey") else {
                 let error = LRRedemptionKitError.invalidInput(inputInformation: "apiKey")
-                call.reject(error.localizedDescription, nil, error, nil)
-                return
-            }
-            
-            guard let vc = self.bridge?.viewController else {
-                let error = LRRedemptionKitError.unknownError
                 call.reject(error.localizedDescription, nil, error, nil)
                 return
             }
@@ -50,6 +46,87 @@ public class RedemptionKitIonicHelperPlugin: CAPPlugin, CAPBridgedPlugin {
             
             if let sessionTimeout = call.getString("sessionTimeout"), let time = Int(sessionTimeout) {
                 timeOut = time
+            }
+            
+            do {
+                try await LRRedemptionKit.initialize(
+                    apiKey: apiKey,
+                    handler: self,
+                    customerID: customerID,
+                    sessionTimeout: timeOut
+                )
+                call.resolve([
+                    "value": "Success"
+                ])
+            } catch {
+                call.reject(error.localizedDescription, nil, error, nil)
+            }
+        }
+    }
+    
+    @objc
+    func launch(_ call: CAPPluginCall) {
+        Task {
+            var redirectionObj: LRRedirectionDetails?
+            if let obj = call.getObject("redirection") {
+                //Go via redirection
+                
+                guard let type = obj["type"] as? String, let typeEnum = LRRedirectionType(rawValue: type) else {
+                    let error = LRRedemptionKitError.invalidInput(inputInformation: "type")
+                    call.reject(error.localizedDescription, nil, error, nil)
+                    return
+                }
+                
+                let title = obj["title"] as? String
+                let value = obj["value"] as? String
+                redirectionObj = LRRedirectionDetails(type: typeEnum, title: title, value: value)
+            }
+            
+            guard let vc = self.bridge?.viewController else {
+                let error = LRRedemptionKitError.unknownError
+                call.reject(error.localizedDescription, nil, error, nil)
+                return
+            }
+            
+            do {
+                try await LRRedemptionKit.launch(
+                    context: vc,
+                    redirection: redirectionObj
+                )
+                call.resolve([
+                    "value": "Success"
+                ])
+            } catch {
+                call.reject(error.localizedDescription, nil, error, nil)
+            }
+        }
+    }
+    
+    @objc
+    func initializeAndLaunch(_ call: CAPPluginCall) {
+        Task {
+            guard let apiKey = call.getString("apiKey") else {
+                let error = LRRedemptionKitError.invalidInput(inputInformation: "apiKey")
+                call.reject(error.localizedDescription, nil, error, nil)
+                return
+            }
+            
+            guard let customerID = call.getString("customerID") else {
+                let error = LRRedemptionKitError.invalidInput(inputInformation: "customerID")
+                call.reject(error.localizedDescription, nil, error, nil)
+                return
+            }
+            
+            var timeOut: Int? = nil
+            
+            if let sessionTimeout = call.getString("sessionTimeout"), let time = Int(sessionTimeout) {
+                timeOut = time
+            }
+            
+            guard let vc = self.bridge?.viewController else {
+                let error = LRRedemptionKitError.unknownError
+                call.reject(error.localizedDescription, nil, error, nil)
+                return
             }
             
             do {
@@ -68,6 +145,173 @@ public class RedemptionKitIonicHelperPlugin: CAPPlugin, CAPBridgedPlugin {
             }
         }
     }
+    
+    @objc
+    func getPointBalance(_ call: CAPPluginCall) {
+        Task {
+            do {
+                let response = try await LRRedemptionKit.getPointBalance(kind: call.getString("kind"))
+                call.resolve(["value": response])
+            } catch {
+                call.reject(error.localizedDescription, nil, error, nil)
+            }
+        }
+    }
+    
+    @objc
+    func getTransactionHistory(_ call: CAPPluginCall) {
+        Task {
+            do {
+                var transactionTypeEnum: LRTransactionType
+                if let transactionType = call.getString("transactionType"), let enumType = LRTransactionType(rawValue: transactionType) {
+                    transactionTypeEnum = enumType
+                } else {
+                    transactionTypeEnum = .all
+                }
+                
+                var startDate: Date?
+                if let startDateInt = call.getInt("startDate") {
+                    startDate = Date(timeIntervalSince1970: TimeInterval(startDateInt))
+                }
+                
+                var endDate: Date?
+                if let endDateInt = call.getInt("endDate") {
+                    endDate = Date(timeIntervalSince1970: TimeInterval(endDateInt))
+                }
+                
+                let response = try await LRRedemptionKit.getTransactionHistory(
+                    transactionType: transactionTypeEnum,
+                    startDate: startDate,
+                    endDate: endDate
+                )
+                call.resolve(["value": response])
+            } catch {
+                call.reject(error.localizedDescription, nil, error, nil)
+            }
+        }
+    }
+    
+    @objc
+    func getRedemptionOptions(_ call: CAPPluginCall) {
+        Task {
+            do {
+                let response = try await LRRedemptionKit.getRedemptionOptions()
+                call.resolve(["value": response])
+            } catch {
+                call.reject(error.localizedDescription, nil, error, nil)
+            }
+        }
+    }
+    
+    @objc
+    func getBanners(_ call: CAPPluginCall) {
+        Task {
+            do {
+                if let moduleName = call.getString("moduleName") {
+                    let response = try await LRRedemptionKit.getBanners(
+                        moduleName: moduleName
+                    )
+                    call.resolve(["value": response])
+                } else {
+                    let response = try await LRRedemptionKit.getBanners()
+                    call.resolve(["value": response])
+                }
+            } catch {
+                call.reject(error.localizedDescription, nil, error, nil)
+            }
+        }
+    }
+    
+    @objc
+    func getGiftVouchers(_ call: CAPPluginCall) {
+        Task {
+            do {
+                let response = try await LRRedemptionKit.getGiftVouchers()
+                call.resolve(["value": response])
+            } catch {
+                call.reject(error.localizedDescription, nil, error, nil)
+            }
+        }
+    }
+    
+    @objc
+    func getOfferCategories(_ call: CAPPluginCall) {
+        Task {
+            do {
+                let response = try await LRRedemptionKit.getOfferCategories()
+                call.resolve(["value": response])
+            } catch {
+                call.reject(error.localizedDescription, nil, error, nil)
+            }
+        }
+    }
+    
+    @objc
+    func getOffers(_ call: CAPPluginCall) {
+        Task {
+            do {
+                guard let typeString = call.getString("type"), let type = LROfferType(rawValue: typeString) else {
+                    let error = LRRedemptionKitError.invalidInput(inputInformation: "type")
+                    call.reject(error.localizedDescription, nil, error, nil)
+                    return
+                }
+                
+                let response = try await LRRedemptionKit.getOffers(
+                    type: type
+                )
+                call.resolve(["value": response])
+            } catch {
+                call.reject(error.localizedDescription, nil, error, nil)
+            }
+        }
+    }
+    
+    @objc
+    func getBillPayCategories(_ call: CAPPluginCall) {
+        Task {
+            do {
+                let response = try await LRRedemptionKit.getBillPayCategories()
+                call.resolve(["value": response])
+            } catch {
+                call.reject(error.localizedDescription, nil, error, nil)
+            }
+        }
+    }
+    
+    @objc
+    func searchProducts(_ call: CAPPluginCall) {
+        Task {
+            guard let typeString = call.getString("type"), let type = LRSearchType(rawValue: typeString) else {
+                let error = LRRedemptionKitError.invalidInput(inputInformation: "type")
+                call.reject(error.localizedDescription, nil, error, nil)
+                return
+            }
+            
+            guard let keyword = call.getString("keyword") else {
+                let error = LRRedemptionKitError.invalidInput(inputInformation: "keyword")
+                call.reject(error.localizedDescription, nil, error, nil)
+                return
+            }
+            
+            do {
+                let response = try await LRRedemptionKit.searchProducts(
+                    type: type,
+                    keyword: keyword
+                )
+                call.resolve(["value": response])
+            } catch {
+                call.reject(error.localizedDescription, nil, error, nil)
+            }
+        }
+    }
+    
+    @objc
+    func clearSession(_ call: CAPPluginCall) {
+        LRRedemptionKit.clearSession()
+        call.resolve([
+            "value": "Success"
+        ])
+    }
 }
 
 extension RedemptionKitIonicHelperPlugin: LRRedemptionKitHandler {
@@ -81,38 +325,3 @@ extension RedemptionKitIonicHelperPlugin: LRRedemptionKitHandler {
         self.notifyListeners("sdkSessionNotifierEvent", data: ["isSessionValid": isSessionValid])
     }
 }
-
-//class CustomViewController: UIViewController, LRSDKDelegate {
-//    func isUserAlive(isUserActive: Bool) {
-//        
-//    }
-//    
-//    func userLogout() {
-//        
-//    }
-//    
-//    func backToHome() {
-//        
-//    }
-//    
-//    func configurationMpinCallBack(orderId: String, requestId: String) {
-//        
-//    }
-//    
-//    let sso: String
-//    
-//    init(sso: String) {
-//        self.sso = sso
-//        super.init(nibName: nil, bundle: nil)
-//    }
-//    
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        
-//        LRSDKConfigurator.launchSDK(ssoToken: sso, delegate: self)
-//    }
-//}
